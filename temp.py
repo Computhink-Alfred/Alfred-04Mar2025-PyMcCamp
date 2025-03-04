6003e822c24b5192076962292ff299790c23992b
Computhink-Alfred <167395775+Computhink-Alfred@users.noreply.github.com>
2025-03-04 07:38:22 +0000

    Mar 4, 2025, 7:38 AM

--- temp.py
+++ temp.py
@@ -1,116 +1,47 @@
-# Task 1: Hello World
-
-# Step 1: Go to “day1.py”
-# Step 2: Use the line print(“hello from Computhink”) to print something
-# Step 3: Run the program by clicking on the “Run” button.
-# You can replace the sentence inside the print() with anything!
-
-###########################################################
-
-# Task 2: More Prints 
-## Continue the below code in day1.py
-
-# Write a program to introduce yourself using print statements
-
-# You should print out
-# - Your name
-# - How old you are
-# - What school do you attend
-# - If you had $2,000 as a reward, what would you buy?
-
-###########################################################
-
-# Task 3: Variable Assignments
-## Continue the below in day1.py
-
-# 1. create 3 variables to hold 3 animals
-# 2. print out the variable values
-
-###########################################################
-
-# Task 4: Math Calculator
-## Continue the below in day1.py
-
-# 1. Create a variable called **var1**. Assign a number to it
-# 2. Create a variable called **var2**. Assign a number to it.
-# 3. Print out the sum of these 2 variables.
-
-###########################################################
-
-# Task 5: String Concatenation
-## Continue the below in day1.py
-
-# 1. Create a variable called **name**. Assign your name to it
-# 2. Create a variable called **food**. Assign your favourite food to it
-# 3. Join the variables with a sentence using the "+" operator
-# 4. Example Output: My name is [John]. I like [bananas]
-
-###########################################################
-
-# Task 6: Python Functions
-## Continue the below in day1.py
-
-# Refer to the code you wrote in task 2 (self introduction)
-
-# 1. Write a function definition called Intro()
-# 2. Copy and paste the code you wrote in task 2 below the function definition.
-# 3. Remember to indent the codes you pasted by 4
-# 4. Call the function Intro in your program
-
-###########################################################
-
-# Task 7: Python Functions with Parameters
-## Continue the below in day1.py
-
-# Write a function called **addition()** that will calculate the addition of 2 numbers. 
-# When you define the function, add in 2 parameters **number1** and **number2**
-
-# In the function, add number1 and number 2
-# print the result out
-
 # Task 1: For Loop to repeat 100 times
 
-You have been punished by your teacher because you have been naughty.
-Write out 100 lines containing "I will not sling mud at my friends."
+# You have been punished by your teacher because you have been naughty.
+# Write out 100 lines containing "I will not sling mud at my friends."
  
-Use a for loop and print to repeat that sentence 100 times
+# Use a for loop and print to repeat that sentence 100 times
 
---------------------------------------------------------------
+###########################################################
+
 # Task 2: For Loop through a string (Make a cheer)
 
-1. create a variable called name, assign your name to it. Use capital letters
-2. using a for loop, loop through each character of your name.
-3. Use string concatenation to make it a cheer
+# 1. create a variable called name, assign your name to it. Use capital letters
+# 2. using a for loop, loop through each character of your name.
+# 3. Use string concatenation to make it a cheer
 
-Example output: 
-Give me a "A"
-Give me a "D"
-Give me a "A"
-Give me a "M"
+# Example output: 
+# Give me a "A"
+# Give me a "D"
+# Give me a "A"
+# Give me a "M"
 
-Who is the best??!?
-ADAM!!
+# Who is the best??!?
+# ADAM!!
 
---------------------------------------------------------
+###########################################################
 
 # Task 3: For Loop - count from 0 to 9
 
-1. Using a for loop, say out the numbers from 0 to 9
+# 1. Using a for loop, say out the numbers from 0 to 9
 
---------------------------------------------------------
+###########################################################
 
 # Task 4: For Loop - count from 1 to 10
 
-1. Using a for loop and the range options, say out the numbers from 1 to 10
+# 1. Using a for loop and the range options, say out the numbers from 1 to 10
 
---------------------------------------------------------
+###########################################################
 
 # Task 5: For Loop - count from 5 to 32
 
-1. Using a for loop and the range options, say out the numbers from 5 to 32
+# 1. Using a for loop and the range options, say out the numbers from 5 to 32
 
---------------------------------------------------------
+###########################################################
 
 # Task 6: For Loop - count down from 50 to 1
 
-1. Using a for loop and the range options, say out the numbers from 50 to 1, counting down
+# 1. Using a for loop and the range options, say out the numbers from 50 to 1, counting down
